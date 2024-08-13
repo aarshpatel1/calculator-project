c361d58cb96820e85b3c3e9a2f9bc6e22cd4dfc9
Aarsh Patel <aarsh18patel@gmail.com>
2024-08-13 15:35:02 +0530

    almost ready

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function appendDigit(value) {
     const display = document.getElementById("display")
+    // const digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.']
+    // if (digits.includes(value)) {
     if (value === "=") {
         display.value = calculation(display.value)
     } else if (value === "C") {
@@ -10,6 +12,9 @@ function appendDigit(value) {
     } else {
         display.value += value
     }
+    // } else {
+    // display.value = removeLastDigit(display.value)
+    // }
 }
 
 function calculation(expression) {
